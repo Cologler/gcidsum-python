efdb04ae86c4dbd76e07de76ffc47852328db1ec
Cologler <skyoflw@gmail.com>
2023-04-29 14:41:15 +0800

    feat: more like sha1sum
    - disable ** recursive

--- gcidsum.py
+++ gcidsum.py
@@ -10,9 +10,20 @@ import sys
 import re
 import pathlib
 import os
+import glob
+
 import xlgcid
 
+class OpenFileError(Exception):
+    def __init__(self, message: str, *args: object) -> None:
+        super().__init__(*args)
+        self.message = str(message)
+
 def _enumerate_paths(pattern: str):
+    if '**' in pattern:
+        # sha1sum does not support recursive.
+        raise OpenFileError(f"can't open '{pattern}': Invalid argument")
+
     try:
         if os.path.isabs(pattern):
             if os.name == 'nt':
@@ -26,14 +37,15 @@ def _enumerate_paths(pattern: str):
                 items = list(pathlib.Path('/').glob(path))
         else:
             try:
-                items = list(pathlib.Path('.').glob(pattern))
+                #items = list(pathlib.Path('.').glob(pattern))
+                items = glob.glob(pattern)
             except re.error:
                 items = [pattern] if os.path.exists(pattern) else None
     except ValueError as ve:
-        __error(ve)
+        raise OpenFileError(ve)
     else:
         if items is None:
-            __error(f"can't open '{pattern}': Invalid argument")
+            raise OpenFileError(f"can't open '{pattern}': Invalid argument")
         else:
             yield from items
 
@@ -48,11 +60,11 @@ def __get_gcid(path):
     try:
         return xlgcid.get_file_gcid_digest(path).hex().lower()
     except PermissionError:
-        __error(f"can't open '{path}': Permission denied")
+        __echo_error_message(f"can't open '{path}': Permission denied")
     except FileNotFoundError:
-        __error(f"can't open '{path}': No such file or directory")
+        __echo_error_message(f"can't open '{path}': No such file or directory")
 
-def __error(msg: str):
+def __echo_error_message(msg: str):
     print(f'gcidsum: {msg}', file=sys.stderr)
 
 def __show_help():
@@ -82,7 +94,7 @@ def __parse_args(args: Tuple[str, ...]):
         if fs:
             excluded, *fs = fs
         else:
-            __error('Missing the gcidsum file (for -e options)')
+            __echo_error_message('Missing the gcidsum file (for -e options)')
             exit()
 
     opt['fs'] = fs
@@ -106,14 +118,14 @@ def gcidsum_main(args: List[str]):
     else:
         excluded = ()
 
-    def is_excluded(name: str):
+    def is_excluded(name) -> bool:
         return str(name) in excluded
 
     if pargs['c']:
         failed = 0
         total = 0
         for path in enumerate_from_args(pargs['fs']):
-            for line in path.read_text('utf-8').splitlines():
+            for line in pathlib.Path(path).read_text('utf-8').splitlines():
                 if line:
                     gcid_in_file, name = __parse_output(line)
 
@@ -128,9 +140,9 @@ def gcidsum_main(args: List[str]):
                         if not pargs['s']:
                             print(f"{name}: {'OK' if eq else 'FAILED'}")
                     elif pargs['w']:
-                        __error(f'invalid format: {line}')
+                        __echo_error_message(f'invalid format: {line}')
         if failed:
-            __error(f'WARNING: {failed} of {total} computed checksums did NOT match')
+            __echo_error_message(f'WARNING: {failed} of {total} computed checksums did NOT match')
     else:
         for path in enumerate_from_args(pargs['fs']):
             if is_excluded(path):
@@ -146,6 +158,9 @@ def main(argv=None):
         if argv is None:
             argv = sys.argv
         return gcidsum_main(tuple(argv[1:]))
+    except OpenFileError as e:
+        __echo_error_message(e.message)
+        return 1
     except KeyboardInterrupt:
         return 1
 
--- tests/test_gcidsum.py
+++ tests/test_gcidsum.py
@@ -6,17 +6,84 @@
 # ----------
 
 import os
+import re
+import subprocess
+import tempfile
+import pathlib
 
-from gcidsum import _enumerate_paths
+import pytest
 
-def test_enumerate_paths():
-    def ep(p: str):
-        return [p.name for p in _enumerate_paths(p)]
+from gcidsum import _enumerate_paths, OpenFileError
 
-    all_py_files = ['gcidsum.py']
+_sha1sum_output_re = re.compile(r'^[0-9a-f]{40}  (?P<fn>.+)$')
 
-    assert all_py_files == ep('*.py')
-    assert all_py_files == ep('./*.py')
-    assert all_py_files == ep('.\\*.py')
+def _get_sha1sum_found_files(pattern: str):
+    proc = subprocess.run(['sha1sum', pattern],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE
+    )
+    proc.check_returncode()
+    outputs = proc.stdout.decode('utf-8')
+    for line in outputs.splitlines():
+        match = _sha1sum_output_re.match(line)
+        assert match, line
+        yield match.group('fn')
 
-    assert all_py_files == ep(os.path.join(os.getcwd(), '*.py'))
+def _get_enumerate_paths_found_files(pattern: str):
+    for path in _enumerate_paths(pattern):
+        yield str(path)
+
+@pytest.mark.parametrize('pattern', [
+    os.path.join(os.getcwd(), '*.py'),
+    'gcidsum.py',
+    '*.py',
+    './*.py',
+    '.\\*.py',
+])
+def test_enumerate_paths(pattern: str):
+    from_enumerate_paths = list(_get_enumerate_paths_found_files(pattern))
+    from_sha1sum = list(_get_sha1sum_found_files(pattern))
+
+    # the separator of from_enumerate_paths is based on OS;
+    # the separator of from_sha1sum is based on inputs
+    # I want to format them with unix path separator
+    from_enumerate_paths = [x.replace('\\', '/') for x in from_enumerate_paths]
+    from_sha1sum = [x.replace('\\', '/') for x in from_sha1sum]
+
+    assert from_enumerate_paths == from_sha1sum
+
+@pytest.mark.parametrize('pattern_suffix', [
+    'test_gcid_data.py',
+    '*.py',
+])
+def test_enumerate_paths_from_abs(pattern_suffix: str):
+    with tempfile.TemporaryDirectory(prefix=__name__ + '-') as tmpdir:
+        (pathlib.Path(tmpdir) / 'test_gcid_data.py').write_text('sometext')
+
+        pattern = os.path.join(tmpdir, pattern_suffix)
+
+        from_enumerate_paths = list(_get_enumerate_paths_found_files(pattern))
+        from_sha1sum = list(_get_sha1sum_found_files(pattern))
+        assert from_enumerate_paths == from_sha1sum
+
+@pytest.mark.parametrize('pattern', [
+    '**/*.py',
+])
+def test_enumerate_paths_with_recursive(pattern: str):
+    e1: OpenFileError = None
+    e2: subprocess.CalledProcessError = None
+    try:
+        list(_get_enumerate_paths_found_files(pattern))
+        assert False
+    except OpenFileError as ee:
+        e1 = ee
+    try:
+        list(_get_sha1sum_found_files(pattern))
+        assert False
+    except subprocess.CalledProcessError as ee:
+        e2 = ee
+
+    assert e1 is not None and e2 is not None
+
+    e2_stderr: bytes = e2.stderr
+    assert e1.message == e2_stderr.decode('utf-8').removeprefix('sha1sum:').strip()
